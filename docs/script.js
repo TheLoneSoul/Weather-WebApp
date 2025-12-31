18692952275a79ede3cb190dea272ebd98803f94
Aabhas Bhandari <aabhasb25@gmail.com>
2025-12-31 17:28:15 +0545

    Add event listener and API setup

--- docs/script.js
+++ docs/script.js
@@ -4,4 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const weather_info = document.getElementById("weather_information");
   const weather_description = document.getElementById("weather_description");
   const error_message = document.getElementById("error_message");
+
+  const API_KEY = "3c9c7f1c48ceac45e8186a1d79b3062e";
+
+  input.addEventListener("keydown", (e) => {
+    const text = input.value.trim();
+    if (e.key === "Enter") {
+      if (!text) return;
+      console.log(text);
+    }
+  });
+
+  function fetchWeatherData(data) {
+    const url = `const url = https://api.openweathermap.org/data/2.5/weather?q=${data}&units=metric&appid=${API_KEY};`;
+  }
+
+  function displayWeatherData(data) {
+    // Display the data
+  }
+
+  function displayError(e) {
+    // Display the error
+  }
 });
