8ec1ef452e21d67b45a1e42e85f17e0874d02ad3
Aabhas Bhandari <aabhasb25@gmail.com>
2025-12-31 17:06:42 +0545

    feat: initialize JavaScript project setup

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const weather_section = document.getElementById("weather_section");
+  const input = document.getElementById("weather_input");
+  const weather_info = document.getElementById("weather_information");
+  const weather_description = document.getElementById("weather_description");
+  const error_message = document.getElementById("error_message");
+});
